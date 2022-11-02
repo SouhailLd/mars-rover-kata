83467a4b2d37907bba03b747bbacf583cf33c7fe
SouhailLd <souhail.lgd@gmail.com>
2022-11-02 12:11:49 +0100

    clean main app class

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,36 +1,7 @@
-import com.mars.rover.kata.entity.Coordinates;
-import com.mars.rover.kata.entity.Obstacle;
-import com.mars.rover.kata.entity.Point;
-import com.mars.rover.kata.enums.DirectionEnum;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import com.mars.rover.kata.service.business.CoordinatesBusinessLogic;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class MainApplication {
 
     public static void main(String[] args) {
-        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        CoordinatesBusinessLogic coordinatesBusinessLogic =
-                appContext.getBean("coordinatesBusinessLogic", CoordinatesBusinessLogic.class);
-        DirectionEnum direction = DirectionEnum.NORTH;
-        Point x = new Point(1, 99);
-        Point y = new Point(99, 99);
-        List<Obstacle> obstacles = Arrays.asList(new Obstacle(20, 20), new Obstacle(30, 30));
-        Coordinates coordinates = Coordinates.builder()
-                .x(x)
-                .y(y)
-                .direction(direction)
-                .obstacles(obstacles).build();
-        coordinatesBusinessLogic.moveRoverForward(coordinates);
-        System.out.println("X= " +coordinates.getX().getLocation()+ " Y= " +coordinates.getY().getLocation());
-        coordinatesBusinessLogic.moveRoverBackward(coordinates);
-        System.out.println("X= " +coordinates.getX().getLocation()+ " Y= " +coordinates.getY().getLocation());
-        coordinates.setDirection(DirectionEnum.EAST);
-        coordinatesBusinessLogic.moveRoverForward(coordinates);
-        System.out.println("X= " +coordinates.getX().getLocation()+ " Y= " +coordinates.getY().getLocation());
+        System.out.println("test cases are hosted under src/test repo!");
     }
 
 }
